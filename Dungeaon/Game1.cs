93faf9fafd0abd02b629578c5c2a2284c5d97b2b
GoastcraftHD <simon.ickler@gmail.com>
2021-09-20 14:10:19 +0200

    Added Text Dialog Field

--- Dungeaon/Game1.cs
+++ Dungeaon/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +11,10 @@ namespace Dungeaon
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        public static SpriteFont font;
+
+        private Texture2D character;
+        private Texture2D whiteRectangle;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,33 +24,79 @@ namespace Dungeaon
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
 
+        private TextBox textBox = new TextBox();
+
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            character = Content.Load<Texture2D>("Bub");
+            font = Content.Load<SpriteFont>("File");
 
-            // TODO: use this.Content to load your game content here
+            whiteRectangle = new Texture2D(GraphicsDevice, 800, 150);
+            Color[] color = new Color[whiteRectangle.Height * whiteRectangle.Width];
+
+            for (int i = 0; i < color.Length; i++)
+            {
+                color[i] = Color.White;
+            }
+
+            whiteRectangle.SetData(color);
+            textBox.textBoxbackground = whiteRectangle; //Content.Load<Texture2D>("TextBack");
         }
+
+        private Vector2 playerPos = new Vector2(100, 100);
+        private float fps = 0f;
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            playerPos = PlayerUtil.Movement(playerPos);
+            fps = 1.0f / (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             base.Update(gameTime);
         }
+
+        private bool a = true;
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            float deltTime = 1.0f / (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            _spriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null, null,
+                null);
+
+            // _spriteBatch.Draw(character, playerPos, Color.White);
+
+            _spriteBatch.DrawString(font, fps.ToString(), new Vector2(1800, 1), Color.White, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+
+            List<String> dialog = new List<String>()
+            {
+                "asdadasdasdagsdafgafasdcasgagdsgyfsdgysadsg\ndfgdsgfsgdfgdfgsgfdgdfgdfgfdgdfgdfgfdgfdgf",
+                "plp llnkokovojicvcdfjidffidjfi iijfiefiefie\nasdaasdadssdaf?"
+            };
+
+            if (a)
+            {
+                if (textBox.DrawTextBox(_spriteBatch, deltTime, character, "Name", dialog))
+                {
+                    textBox.Reset();
+                    a = false;
+                }
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Dungeaon/TextBox.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Dungeaon
+{
+    class TextBox
+    {
+        public Texture2D textBoxbackground;
+        private float timeSinceLastIncrement = 0;
+        private int dialogIndex = 0;
+        int wordIndex = 0;
+        private String text = "";
+
+        public bool DrawTextBox(SpriteBatch _spriteBatch, float deltaTime, Texture2D characterHead, String characteName, List<String> dialog)
+        {
+            _spriteBatch.Draw(textBoxbackground, new Vector2(560, 910), Color.White);
+            _spriteBatch.Draw(characterHead, new Vector2(560, 910), Color.White);
+
+
+            String dia = dialog[dialogIndex];
+
+            if (wordIndex < dia.Length)
+            {
+                timeSinceLastIncrement += deltaTime;
+
+                if (timeSinceLastIncrement >= .5)
+                {
+                    text += dia[wordIndex];
+                    wordIndex++;
+                    timeSinceLastIncrement = 0;
+                }
+            }
+            else
+            {
+                _spriteBatch.DrawString(Game1.font, "Press 'E' to continue.", new Vector2(1095, 1020), Color.Black, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+
+                if (dialogIndex + 1 < dialog.Count)
+                {
+                    if (Keyboard.GetState().IsKeyDown(Keys.E))
+                    {
+                        if (dialogIndex + 1 < dialog.Count)
+                        {
+                            dialogIndex++;
+                            int diaIndex = dialogIndex;
+                            Reset();
+                            dialogIndex = diaIndex;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                        
+                    }
+                }
+            }
+            _spriteBatch.DrawString(Game1.font, text, new Vector2(700, 910), Color.Black, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+            return false;
+        }
+
+        public void DrawTextBox(SpriteBatch _spriteBatch, float deltaTime, Texture2D characterHead, String characteName, String dialog)
+        {
+            List<String> dialogList = new List<string>() { dialog };
+            DrawTextBox(_spriteBatch, deltaTime, characterHead, characteName, dialogList);
+        }
+
+        public void Reset()
+        {
+            wordIndex = 0;
+            dialogIndex = 0;
+            timeSinceLastIncrement = 0;
+            text = "";
+        }
+    }
+}
